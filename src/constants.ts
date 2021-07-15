bfb54acc02640357d8526c8503124cc67fe5276f
Pinosto <juho.pinosto@hotmail.com>
2021-07-15 11:45:44 +0000

    85 login service error handling

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export enum StatusCodes {
+    Code200 = 200,
+}
+
+export enum LoginMessages {
+    RegistrationSuccees = "Registration Succees",
+    RegistrationFail = "Unable to Successfully Complete Registration ...Try Again",
+}
+
+export enum Routes {
+    login = "login",
+    register = "register",
+    usersMe = "users/me",
+}
